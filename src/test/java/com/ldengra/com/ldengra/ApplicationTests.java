208f5d7f3efae26a353ae3c56b3298f76054e3b2
ldengra <hiker_luis@hotmail.com>
2022-01-16 01:02:40 +0100

    commit de codigo

---
+++ src/test/java/com/ldengra/com/ldengra/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldengra.com.ldengra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
